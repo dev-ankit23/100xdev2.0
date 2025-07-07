0efba76d74c99b962a5730a70c8ae861effc11f8
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-07 11:38:56 +0530

    middleware assingment done

--- Week5/index.js
+++ Week5/index.js
@@ -2,32 +2,22 @@ const express = require("express");
 
 const app = express();
 
+function logger(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  const timestamp = new Date();
+
+  console.log(`The Method is ${method} for the url ${url} and at ${timestamp}`);
+  next();
+}
+
+app.use(logger);
+
 app.get("/sum", function (req, res) {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
 
   res.send(a + b);
-});
-
-app.get("/multiply", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  res.send(a * b);
-});
-
-app.get("/divide", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  res.send(a / b);
-});
-
-app.get("/subtract", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-
-  res.send(a - b);
 });
 
 app.listen(3000, () => {
