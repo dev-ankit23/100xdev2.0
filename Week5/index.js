57adef571b586ddae302f0be90cc378bd180e3d8
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-07 12:39:14 +0530

    Cors , Middelware

--- Week5/index.js
+++ Week5/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const app = express();
 
-function logger(req, res, next) {
-  const method = req.method;
-  const url = req.url;
-  const timestamp = new Date();
+// function logger(req, res, next) {
+//   const method = req.method;
+//   const url = req.url;
+//   const timestamp = new Date();
 
-  console.log(`The Method is ${method} for the url ${url} and at ${timestamp}`);
-  next();
-}
+//   console.log(`The Method is ${method} for the url ${url} and at ${timestamp}`);
+//   next();
+// }
 
-app.use(logger);
+// app.use(logger);
 
 app.get("/sum", function (req, res) {
   const a = parseInt(req.query.a);
