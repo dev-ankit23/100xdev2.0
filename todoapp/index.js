feac83169ae764005f49a80e1c65a2c3fb822330
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-17 07:08:53 +0530

    route setup

--- todoapp/index.js
+++ todoapp/index.js
@@ -3,12 +3,30 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
+let users = [];
+let todos = [];
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (users.username === username) {
+    res.json({
+      message: "User Already Exist",
+    });
+  }
+  users.push({
+    username: username,
+    password: password,
+  });
+  res.json({
+    message: "Account Created ",
+  });
+});
 app.post("/signin", (req, res) => {});
 
 app.post("/create", (req, res) => {});
