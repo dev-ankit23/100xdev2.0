4273db7504064f474e549e8bf5170a928aaf6d92
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-18 12:15:21 +0530

    singup , signin , saving in DB

--- Week7/todo/index.js
+++ Week7/todo/index.js
@@ -1,10 +1,55 @@
 const express = require("express");
 const app = express();
+const { UserModel, TodoModel } = require("./db");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const JWT_SECERT = "ankithello";
+mongoose
+  .connect(
+    "mongodb+srv://ankitmauryacoder:nqZrFW1HTnBMEFTo@cluster0.akv6x97.mongodb.net/TodoDB"
+  )
+  .then(() => console.log("Connected!"));
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {});
-app.post("/login", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  UserModel.create({
+    username: username,
+    email: email,
+    password: password,
+  });
+
+  res.json({
+    message: "Account Created Sucessfully",
+  });
+});
+app.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const response = await UserModel.findOne({
+    email: email,
+    password: password,
+  });
+  if (response) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_SECERT
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid Email or Password",
+    });
+  }
+});
 app.post("/todo", (req, res) => {});
 app.get("/todos", (req, res) => {});
 
