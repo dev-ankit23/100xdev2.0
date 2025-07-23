e45c5d6095fde83b6b3cf51c4876f3a98285bb94
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-23 08:13:46 +0530

    usestate,useeffect

--- Week9/React-week-9/src/App.jsx
+++ Week9/React-week-9/src/App.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return (
-    <>
-      <div>Hello There</div>
-    </>
-  );
+  const [count, setCount] = useState(1);
+
+  function increaseCount() {
+    setCount((currentVal) => currentVal + 1);
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(increaseCount, 1000); // set once
+
+    // Cleanup on unmount
+    return () => clearInterval(intervalId);
+  }, []); // Empty array: only run once on mount
+
+  return <>{count}</>;
 }
 
 export default App;
