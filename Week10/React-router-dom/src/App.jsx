6ec306fd8a4ec76f3d8ed2364fe74091257eb6a5
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-29 09:12:11 +0530

    react-router-dom-routes

--- Week10/React-router-dom/src/App.jsx
+++ Week10/React-router-dom/src/App.jsx
@@ -1,8 +1,38 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Class11Program() {
+  return (
+    <div>
+      <h1>Class - 11 Program</h1>
+    </div>
+  );
+}
+
+function Class12Program() {
+  return (
+    <div>
+      <h1>Class - 12 Program</h1>
+    </div>
+  );
+}
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to Landingpage</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <>
-      <h1>Hello There</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/class11" element={<Class11Program />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/class12" element={<Class12Program />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
