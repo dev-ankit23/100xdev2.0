78035702f1c1a1ad063d04bb3c7ad89699abe7bc
devankitx <126304960+devankitx@users.noreply.github.com>
2025-07-30 10:38:19 +0530

    typescript

--- Week14/index.js
+++ Week14/index.js
@@ -1,23 +1,19 @@
+"use strict";
 // Gretting Fucntion
-
 // function greeting(firstname: string) {
 //   console.log(`Hello ${firstname}`);
 // }
-
 // greeting("Ankit");
-
 // function SumTwoValue(a: number, b: number) {
 //   return a + b;
 // }
-
 // console.log(SumTwoValue(23, 34));
-
-// function isLegal(age: number) {
-//   if (age > 18) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// console.log(isLegal(34));
+function isLegal(age) {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isLegal(34));
