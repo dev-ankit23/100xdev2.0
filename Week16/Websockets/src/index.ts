f61353fcb6a5d9e570648110f3a58014be8bd8bf
devankitx <126304960+devankitx@users.noreply.github.com>
2025-08-04 10:54:18 +0530

    websocket server

--- Week16/Websockets/src/index.ts
+++ Week16/Websockets/src/index.ts
@@ -3,6 +3,13 @@ import WebSocket, { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function (socket) {
-  console.log("Websocket Server Connected");
-  socket.send("Hello Websocket Server");
+  console.log("Server Connected");
+
+  socket.on("message", function (e) {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    } else {
+      socket.send("Enter ping to get the value");
+    }
+  });
 });
