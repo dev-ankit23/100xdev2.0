4d0f90caa8f9833ea80f7de97e4985f04b38faa6
devankitx <126304960+devankitx@users.noreply.github.com>
2025-08-18 11:44:48 +0530

    postgres sql , pg , sql injection

---
+++ Week17/dist/index.js
@@ -0,0 +1,14 @@
+import { Client } from "pg";
+const pgClient = new Client({
+    connectionString: "postgresql://neondb_owner:npg_vbXNgGM5U7Ij@ep-hidden-credit-ae44a37k-pooler.c-2.us-east-2.aws.neon.tech/neondb",
+    ssl: {
+        rejectUnauthorized: true,
+    },
+});
+async function main() {
+    await pgClient.connect();
+    const result = await pgClient.query("SELECT * FROM USERS;");
+    console.log(result.rows);
+}
+main();
+//# sourceMappingURL=index.js.map
