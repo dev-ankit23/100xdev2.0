74fb54a131718b25706716a4b951f0f82c300644
devankitx <ankitmaurya.coder@gmail.com>
2025-08-20 18:54:28 +0530

    Prisma , ORM , Postgress

--- Week18/dist/index.js
+++ Week18/dist/index.js
@@ -1,4 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello there");
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+const Client = new PrismaClient();
+const app = express();
+app.use(express.json());
+app.post("/createuser", async (req, res) => {
+    await Client.user.create({
+        data: req.body,
+    });
+    res.json({
+        message: "User Created",
+    });
+});
+// CreateUser();
+app.listen(3000, () => {
+    console.log("App is listening....");
+});
 //# sourceMappingURL=index.js.map
--- Week18/src/index.ts
+++ Week18/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello there");
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const Client = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post("/createuser", async (req, res) => {
+  await Client.user.create({
+    data: req.body,
+  });
+  res.json({
+    message: "User Created",
+  });
+});
+
+// CreateUser();
+
+app.listen(3000, () => {
+  console.log("App is listening....");
+});
